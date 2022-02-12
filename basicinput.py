754fc7e7da18c808fa7fb025b6443484fde2af4e
srilekhananabala19 <99317080+srilekhananabala19@users.noreply.github.com>
2022-02-12 11:38:20 +0530

    Add files via upload

---
+++ basicinput.py
@@ -0,0 +1,21 @@
+# a=input()
+# print(a,type(a))
+# print("The value of a is "+a)
+
+# b=input()
+# print(b,type(b))
+# print("The value of b is "+b)
+
+# c=int(input('Enter your Phone Number:'))
+# print(c,type(c))
+# print("The value of c is"+c)
+
+# d=float(input('Enter your height:'))
+# print(d,type(d))
+# print("The value of d is",d)
+
+# e=input('Enter your name:')
+# print(e,type(e))
+# print("Your name is:",e)
+# print("Your name is:"+' '+e)
+# print('Your name is:'+chr(32)+e)
